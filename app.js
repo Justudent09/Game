a615e84db3c9b559edee5aa6ea63ed2c7cf4d16b
Justudent <justudent09@mail.ru>
2026-04-21 16:18:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -40,10 +40,6 @@ function initTournament() {
         players = savedData.players;
     } else {
         players = [...TOURNAMENT_PLAYERS];
-        for (let i = players.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [players[i], players[j]] = [players[j], players[i]];
-        }
     }
 
     // Расчет базовой высоты (для пары)
